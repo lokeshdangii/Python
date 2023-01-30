1b241dd917c27bd343b470ce00be992bbfd09e0d
lokeshdangii <lokeshdangi1045@gmail.com>
2023-01-30 17:22:55 +0530

    absolutedifference

---
+++ absdiff.py
@@ -0,0 +1,24 @@
+arr = [9,4,1,6,1,6,35,8]
+difflist = []
+
+for i in range(0,len(arr)):
+
+	if i == len(arr)-1:
+		break
+	else:
+		diff = arr[i+1]-arr[i]
+		difflist.append(diff)
+
+
+print("list is :", difflist)
+
+max1 = max(difflist)
+maxpos = difflist.index(max(difflist))
+print(" maximum position at index of list:",maxpos)
+print("The maximum absolute difference is :",max1)
+
+print(arr[maxpos])
+print(arr[maxpos+1])
+
+print("{} is the absolute difference of {} and {}".format(max1,arr[maxpos],arr[maxpos+1]))
+
--- leapyear.py
+++ leapyear.py
@@ -1,20 +1,6 @@
 year = int(input("Enter year:"))
 
-# if year%4==0:
-# 	print("Given year {} is leap".format(year))
-# 	if year%100!=0:
-# 		print("Given year{} is leap".format(year))
-# 	else:
-# 		if year%400==0:
-# 			print("Given year {} is leap".format(year))
-# 		else:
-# 			print("not leap year")
-# else:
-# 	print("Not")
-
-if year%400==0 and year%100==0 or year%4==0:
-	print("year{} is leap".format(year))
-elif year%4==0 and year%100!=0:
-	print("year{} is leap".format(year))
+if year%4==0 and year%400==0 and year%100 == 0:
+	print("leap year")
 else:
-	print("year{} is not leap".format(year))
+	print("not a leap year")
