8bcd2bb5eef047e1e6a8c1881ab0b4b9a8e49ad0
lokeshdangii <lokeshdangi1045@gmail.com>
2023-02-03 18:49:53 +0530

    class

---
+++ OOPS/100circle.py
@@ -0,0 +1,36 @@
+class circle:
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return 3.14*self.radius*self.radius
+
+    def circumference(self):
+        return 3.14 *2*self.radius
+
+def dwrite(x):
+    file1 = open("detail100.txt","a")
+    file1.write(x)
+    file1.close()
+    print("write operation done successfully")
+
+def dread():
+    pass
+
+for i in range(1,101):
+    var1 = circle(i)
+    var2 = var1.area()
+    var3 = var1.circumference()
+    var4 = "{},{},{}\n".format(var1.radius,var2,var3)
+
+    dwrite(var4)
+    print("Radius of circle = {} cm".format(var1.radius))    
+    print("Area:{} sq cm".format(var2))
+    print("Circumference: {} cm".format(var3))
+    print("-------------------------------")
+
+
+
+
+
+
+
