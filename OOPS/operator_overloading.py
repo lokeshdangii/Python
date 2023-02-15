c5f58854185bc553eed9d36830c36e8aa411dba8
lokeshdangii <lokeshdangi1045@gmail.com>
2023-02-15 19:20:08 +0530

    operator overloading

---
+++ OOPS/operator_overloading.py
@@ -0,0 +1,27 @@
+# on the basis of behaviour of operand the operator is performing different operation.
+
+# class Test:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+    
+#     def show(self):
+#         print("Part A:",self.a)
+#         print("Part B",self.b)
+
+#     def __add__(self,other):
+#         x = self.a + other.a
+#         y = self.b + other.b
+#         z = Test(x,y)
+#         return z
+    
+# s1 = Test(24,25)
+# s2 = Test(45,50)
+
+# s3 = s1 + s2
+# s3.show()
+
+# a = 5
+# b = 6
+# c = a.__add__(b)
+# print(c) 
