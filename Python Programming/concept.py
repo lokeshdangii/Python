4856d653f5aab237da59254122a687ee253f395e
lokeshdangii <lokeshdangi1045@gmail.com>
2023-02-26 17:21:24 +0530

    basic

---
+++ Python Programming/concept.py
@@ -0,0 +1,21 @@
+# a,b,c,d = 10,12,13,14
+# print(a,b,c,d, end = " ")
+
+# a,b,c = [1,2],[1,2,3,4],[1,2,3,5,6,78,]
+# print(a,b,c, end = " ")
+
+# a,b,c = [1,2]
+# print(a,b,c)
+
+# a = 3,4,5
+# print(a)
+# print(type(a))
+
+t1 = ("python")
+print(t1)
+print(type(t1))
+
+t2 = ("python",)
+print(t2)
+print(type(t2))
+
