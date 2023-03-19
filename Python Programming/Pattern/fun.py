01eecdd1c8653299d3cf9dc9659ada73b16db611
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-19 14:52:27 +0530

    Inner function

--- Python Programming/Pattern/fun.py
+++ Python Programming/Pattern/fun.py
@@ -1,10 +1,24 @@
-def f1():
-    print("Hello")
-    def f2():
-        print("Anna")
-    return f2()
+# def f1():
+#     print("Hello")
+#     def f2():
+#         print("Anna")
+#     return f2()
 
 
 
-str = f1()
-print("f2 = ",str)
+# str = f1()
+# print("f2 = ",str)
+
+def enhanced(func):
+    def inner(a,b):
+        if b==0:
+            return "can not divide by zero"
+        else:
+            return func(a,b)
+    return inner  
+@enhanced
+def f1(a,b):
+    return a/b
+
+a = f1(10,0)
+print(a)
