ee60cc69aafaf897cf6b3234d4252f5313f82f97
lokeshdangii <lokeshdangi1045@gmail.com>
2023-03-24 18:47:01 +0530

    Python

--- Python Programming/basic.py
+++ Python Programming/basic.py
@@ -1,5 +1,5 @@
-# a,b,c,d = 10,12,13,14
-# print(a,b,c,d, end = " ")
+a,b,c,d = 10,12,13,14
+print(a,b,c,d, end = " ")
 
 # a,b,c = [1,2],[1,2,3,4],[1,2,3,5,6,78,]
 # print(a,b,c, end = " ")
@@ -7,6 +7,6 @@
 # a,b,c = [1,2]
 # print(a,b,c)
 
-a = 3,4,5
-print(a)
-print(type(a))
+# a = 3,4,5
+# print(a)
+# print(type(a))
