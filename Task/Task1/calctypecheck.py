ac4f3d17a206907c61eb2494a1e550b6eb9ebfdb
lokeshdangii <lokeshdangi1045@gmail.com>
2023-08-09 15:47:38 +0530

    program to check and typecast value to int or float

---
+++ Task/Task1/calctypecheck.py
@@ -0,0 +1,24 @@
+# program to check the value given by user and conver it accordingly to int or float
+
+def typecheck(user_value): #function for checking the user value
+
+	num_char = ["0","1","2","3","4","5","6","7","8","9"]
+	num_count = 0
+	dot_count = 0
+
+	for i in user_value:
+		if i in num_char:
+			num_count += 1
+		
+		elif i ==".":
+			dot_count +=1
+		
+
+	if num_char==1 and dot_count ==0:
+		user_value = int(user_value)
+
+	else:
+		user_value = float(user_value)
+
+	return user_value
+
