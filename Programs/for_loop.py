afa0028382062fbb57e079902cc19bedc1cfbb2e
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-07 13:08:55 +0530

    for loop

---
+++ Programs/for_loop.py
@@ -0,0 +1,13 @@
+for i in range(1,5):
+    
+    print("value or i = ", i,end = " ")
+ 
+    
+    for j in range(i-1,-1,-1):
+        print()
+        print(j, end = "")
+        
+    print()
+        
+        
+        
