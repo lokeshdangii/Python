99b6b1ec2517c53c764bed254fbdf6920790401e
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-08 13:35:44 +0530

    python program to calculate average

--- Programs/avg.py
+++ Programs/avg.py
@@ -1,7 +1,24 @@
+# choice = int(input("Enter no. of terms:"))
+# sum = 0
+# for i in range(choice):
+# 	value = int(input("Enter value : "))
+# 	sum = sum + value
+# average = sum/choice
+# print("Average=",average)
+
 choice = int(input("Enter no. of terms:"))
 sum = 0
-for i in range(choice):
-	value = int(input("Enter value : "))
+value = 5
+
+i = 0
+while(i <= choice ):
 	sum = sum + value
+	i = i+ 1
+	print(sum)
+ 
 average = sum/choice
 print("Average=",average)
+ 
+ 
+
+				
