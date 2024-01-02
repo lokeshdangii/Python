d558f92eb39a093c290fab4257814f5f3ffaf5b0
lokeshdangii <lokeshdangi1045@gmail.com>
2024-01-02 16:37:54 +0530

    Stack Sorting

--- Stack/07_sort_stack.py
+++ Stack/07_sort_stack.py
@@ -23,7 +23,7 @@ class Stack:
 stack1 = Stack()  # main stack
 stack2 = Stack()  #stack to store elements temporarily
 
-numbers = int(input("Enter no. of elements to store in stack"))
+numbers = int(input("Enter no. of elements to store in stack : "))
 
 for i in range(numbers):
     element = int(input("Enter element to store in stack : "))
@@ -31,6 +31,7 @@ for i in range(numbers):
     # if element is greater than push directly
     if stack1.size() > 0 and element > stack1.peek():
         stack1.push(element)
+        print(stack1.display())
 
     # if element is small than the top element of stack1 
     elif stack1.size() > 0 and element < stack1.peek():
@@ -40,14 +41,17 @@ for i in range(numbers):
             stack2.push(stack1.pop())
 
         stack1.push(element)
+        print(stack1.display())
 
         # again pushing the elements of stack2 to stack1
         while(stack2.size() > 0):
             stack1.push(stack2.pop())
+            print(stack1.display())
 
     # condition to push the first element in the stack
     elif stack1.size() == 0:
         stack1.push(element)
+        print(stack1.display())
 
 
 print(stack1.display())
